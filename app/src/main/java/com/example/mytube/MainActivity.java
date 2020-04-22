e68f36e6567daff8b62098b3b9ce1f74d8ec9d3a
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-04-22 15:52:36 +0200

    fik knappen sang liste til a funger

--- app/src/main/java/com/example/mytube/MainActivity.java
+++ app/src/main/java/com/example/mytube/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.mytube;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button queue_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        queue_Button=findViewById(R.id.Sang_Liste_Knap);
+        queue_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.queue_list);
+            }
+        });
     }
 }
