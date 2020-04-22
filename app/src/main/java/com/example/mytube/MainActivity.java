00cd53c348fb7cdd98906da7a3cb6bc3585a6645
Matiaslys <mati1857@edu.easj.dk>
2020-04-22 16:39:12 +0200

    Now we can switch between the 3 windows/xml class.

--- app/src/main/java/com/example/mytube/MainActivity.java
+++ app/src/main/java/com/example/mytube/MainActivity.java
@@ -9,16 +9,27 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button queue_Button;
+    Button songButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        queue_Button=findViewById(R.id.Sang_Liste_Knap);
+
+        queue_Button = findViewById(R.id.Sang_Liste_Knap);
+        songButton = findViewById(R.id.Mine_Sange_knap);
+
         queue_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 setContentView(R.layout.queue_list);
+            }
+        });
+
+        songButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.song_list);
             }
         });
     }
