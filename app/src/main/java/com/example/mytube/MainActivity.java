b4c371af5c9ead15de85090e03a79fe2f4f8ef27
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-04-24 14:25:10 +0200

    made permission to external storage

--- app/src/main/java/com/example/mytube/MainActivity.java
+++ app/src/main/java/com/example/mytube/MainActivity.java
@@ -1,47 +1,69 @@
 package com.example.mytube;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button queue_Button;
-    Button songButton;
+    private static final int MY_PERMISSION_REQUEST = 1;
+    ArrayList<String> arrayList;
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    Button queue_Button, songButton;
 
     public void onstart() {
-
         queue_Button = findViewById(R.id.Sang_Liste_Knap);
         songButton = findViewById(R.id.Mine_Sange_knap);
-
         queue_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 setContentView(R.layout.queue_list);
             }
         });
-
         songButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setContentView(R.layout.song_list);
             }
         });
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST);
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST);
+            }
+        } else {
+            doStuff();
+        }
+    }
+
+    public void doStuff() {
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         onstart();
     }
 
     public void onBackPressed() {
         setContentView(R.layout.activity_main);
-
         onstart();
     }
 }
