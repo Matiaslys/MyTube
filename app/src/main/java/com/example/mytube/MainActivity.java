1d49d0fa78d29811f21a52a28396e981ae65bbdc
Matiaslys <mati1857@edu.easj.dk>
2020-05-01 17:56:40 +0200

    Test

--- app/src/main/java/com/example/mytube/MainActivity.java
+++ app/src/main/java/com/example/mytube/MainActivity.java
@@ -3,13 +3,29 @@ package com.example.mytube;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button queue_Button, songButton;
+    private Button queue_Button, songButton;
+    private ImageButton pause, previous, next;
+    private SeekBar seek_Bar;
+    private MediaPlayer mediaPlayer;
+    private int Song;
+    private ArrayList<File> mySongs;
+    private String mSongName;
+    private TextView songNameTxt;
+    private int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +33,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         queue_Button = findViewById(R.id.Sang_Liste_Knap);
         songButton = findViewById(R.id.Mine_Sange_knap);
+        seek_Bar = findViewById(R.id.seekBar);
+        pause = findViewById(R.id.pause);
+        previous = findViewById(R.id.previous);
+        next = findViewById(R.id.next);
+//        validateReceiveValuesAndStartPlaying();
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                playPauseSong();
+            }
+        });
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.getCurrentPosition() > 0) {
+//                    playPreviousSong();
+                }
+            }
+        });
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.getCurrentPosition() > 0) {
+//                    playNextSong();
+                }
+            }
+        });
         queue_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,7 +74,63 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+//        Bundle bundle = getIntent().getExtras();
+//        int[] songs = bundle.getIntArray("song");
+//        String[] songsNameAndArtistArray = bundle.getStringArray("SongName");
+
     }
+
+//    private void validateReceiveValuesAndStartPlaying() {
+//        if (mediaPlayer != null) {
+//            mediaPlayer.stop();
+//            mediaPlayer.release();
+//        }
+//        Intent intent = getIntent();
+//        Bundle bundle = intent.getExtras();
+//        mySongs = (ArrayList) bundle.getParcelableArrayList("song");
+//        mSongName = mySongs.get(position).getName();
+//        String songName = intent.getStringExtra("name");
+//        songNameTxt.setText(songName);
+//        songNameTxt.setSelected(true);
+//        position = bundle.getInt("position", 0);
+//        Uri uri = Uri.parse(mySongs.get(position).toString());
+//        mediaPlayer = MediaPlayer.create(MainActivity.this, uri);
+//        mediaPlayer.start();
+//    }
+//
+//    private void playPauseSong() {
+//        if (mediaPlayer.isPlaying()) {
+//            mediaPlayer.pause();
+//        } else {
+//            mediaPlayer.start();
+//        }
+//    }
+//
+//    private void playNextSong() {
+//        mediaPlayer.pause();
+//        mediaPlayer.stop();
+//        mediaPlayer.release();
+//        position = ((position + 1) % mySongs.size());
+//        Uri uri = Uri.parse(mySongs.get(position).toString());
+//        mediaPlayer = MediaPlayer.create(MainActivity.this, uri);
+//        mSongName = mySongs.get(position).toString();
+//        songNameTxt.setText(mSongName);
+//        mediaPlayer.start();
+//    }
+//
+//    private void playPreviousSong() {
+//        mediaPlayer.pause();
+//        mediaPlayer.stop();
+//        mediaPlayer.release();
+//        position = ((position - 1) < 0 ? (mySongs.size() - 1) : (position - 1));
+//        Uri uri = Uri.parse(mySongs.get(position).toString());
+//        mediaPlayer = MediaPlayer.create(MainActivity.this, uri);
+//        mSongName = mySongs.get(position).toString();
+//        songNameTxt.setText(mSongName);
+//        mediaPlayer.start();
+//    }
 
     public void onBackPressed() {
         Intent intent = new Intent(MainActivity.this, MainActivity.class);
--- app/src/main/java/com/example/mytube/Song_List.java
+++ app/src/main/java/com/example/mytube/Song_List.java
@@ -7,12 +7,14 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.ContentResolver;
 import android.content.ContentUris;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.AdapterView;
@@ -20,10 +22,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Song_List extends AppCompatActivity {
+    private String[] itemsAll;
     private static final int MY_PERMISSION_REQUEST = 1;
     ArrayList<String> arrayList;
     MediaPlayer mediaPlayer = new MediaPlayer();
@@ -47,9 +51,25 @@ public class Song_List extends AppCompatActivity {
                 ActivityCompat.requestPermissions(Song_List.this,
                         new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST);
             }
-        } else {
-            doStuff();
         }
+    }
+
+    public ArrayList<File> readOnlyAudioSongs(File file) {
+        ArrayList<File> arrayList = new ArrayList<>();
+        File[] allFiles = file.listFiles();
+        for (File individualFile : allFiles) {
+            if (individualFile.isDirectory() && !individualFile.isHidden()) {
+                arrayList.addAll(readOnlyAudioSongs(individualFile));
+            } else {
+                if (individualFile.getName().endsWith(".mp3") ||
+                        individualFile.getName().endsWith(".aac") ||
+                        individualFile.getName().endsWith(".wav") ||
+                        individualFile.getName().endsWith(".wma")) {
+                    arrayList.add(individualFile);
+                }
+            }
+        }
+        return arrayList;
     }
 
     public void getMusic() {
@@ -73,47 +93,75 @@ public class Song_List extends AppCompatActivity {
     }
 
     public void doStuff() {
-        listView = (ListView) findViewById(R.id.listView);
-        arrayList = new ArrayList<>();
-        getMusic();
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
-        listView.setAdapter(adapter);
-        int index = 0;
-        final long[] songs = new long[list.size()];
-        for (final Long value : list) {
-            songs[index++] = value;
+//        listView = (ListView) findViewById(R.id.listView);
+//        arrayList = new ArrayList<>();
+//        getMusic();
+        final ArrayList<File> audioSongs = readOnlyAudioSongs(Environment.getExternalStorageDirectory());
+        itemsAll = new String[audioSongs.size()];
+        for (int songCounter = 0; songCounter < audioSongs.size(); songCounter++) {
+            itemsAll[songCounter] = audioSongs.get(songCounter).getName();
         }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(Song_List.this, android.R.layout.simple_list_item_1, itemsAll);
+        listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if (mediaPlayer.isPlaying()) {
-                    mediaPlayer.reset();
-                }
-                Uri contentUri = ContentUris.withAppendedId(
-                        android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, songs[i]);
-                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                try {
-                    mediaPlayer.setDataSource(getApplicationContext(), contentUri);
-                    mediaPlayer.prepare();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                    @Override
-                    public void onPrepared(MediaPlayer mp) {
-                        mediaPlayer.start();
-                    }
-                });
-                /////////////////
-//                Intent intent = new Intent(Song_List.this, MainActivity.class);
-//                startActivity(intent);
-//                listSongs.get(i).setAudioStreamType(AudioManager.STREAM_MUSIC);
-                //                    listSongs.get(i).setDataSource(getApplicationContext(), contentUri);
-//                    listSongs.get(i).prepare();
-//                listSongs.get(i).start();
-                //open music player to play desired song
+                String songName = listView.getItemAtPosition(i).toString();
+                Intent intent = new Intent(Song_List.this, MainActivity.class);
+                intent.putExtra("song", audioSongs);
+                intent.putExtra("name", songName);
+                intent.putExtra("position", i);
+                startActivity(intent);
             }
         });
+
+//        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+//        listView.setAdapter(adapter);
+//        int songNameAndArtist = 0;
+//        final String[] nameAndArtist = new String[arrayList.size()];
+//        for (final String value : arrayList) {
+//            nameAndArtist[songNameAndArtist++] = value;
+//        }
+//        int SongID = 0;
+//        final long[] songs = new long[list.size()];
+//        for (final Long value : list) {
+//            songs[SongID++] = value;
+//        }
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Intent intent = new Intent(Song_List.this, MainActivity.class);
+//                intent.putExtra("SongName", nameAndArtist);
+//                intent.putExtra("Song", songs);
+//                startActivity(intent);
+//                if (mediaPlayer.isPlaying()) {
+//                    mediaPlayer.reset();
+//                }
+//                Uri contentUri = ContentUris.withAppendedId(
+//                        android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, songs[i]);
+//                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+//                try {
+//                    mediaPlayer.setDataSource(getApplicationContext(), contentUri);
+//                    mediaPlayer.prepare();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+//                    @Override
+//                    public void onPrepared(MediaPlayer mp) {
+//                        mediaPlayer.start();
+//                    }
+//                });
+//                ///////////////
+////                Intent intent = new Intent(Song_List.this, MainActivity.class);
+////                startActivity(intent);
+////                listSongs.get(i).setAudioStreamType(AudioManager.STREAM_MUSIC);
+////                                    listSongs.get(i).setDataSource(getApplicationContext(), contentUri);
+////                    listSongs.get(i).prepare();
+////                listSongs.get(i).start();
+////                open music player to play desired song
+//            }
+//        });
     }
 
     @Override
